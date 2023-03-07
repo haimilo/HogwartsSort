bed0e96104e89983187b5e068d7e794eecbe5c3e
Harry <nthai7@cmcglobal.vn>
2023-03-07 14:07:34 +0700

    first commit

--- src/webparts/hogwartsShort/components/IHogwartsShortProps.ts
+++ src/webparts/hogwartsShort/components/IHogwartsShortProps.ts
@@ -1,7 +1,10 @@
+import { WebPartContext } from "@microsoft/sp-webpart-base";
 export interface IHogwartsShortProps {
   description: string;
-  isDarkTheme: boolean;
-  environmentMessage: string;
-  hasTeamsContext: boolean;
+  // isDarkTheme: boolean;
+  // environmentMessage: string;
+  // hasTeamsContext: boolean;
   userDisplayName: string;
+  context: WebPartContext;
+  userDisplayEmail: string;
 }
