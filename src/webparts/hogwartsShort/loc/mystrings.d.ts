55f5bc365363b8e96372c5322e3b9b88728a5e52
Harry <nthai7@cmcglobal.vn>
2023-03-07 13:46:26 +0700

    first commit

---
+++ src/webparts/hogwartsShort/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IHogwartsShortWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'HogwartsShortWebPartStrings' {
+  const strings: IHogwartsShortWebPartStrings;
+  export = strings;
+}
