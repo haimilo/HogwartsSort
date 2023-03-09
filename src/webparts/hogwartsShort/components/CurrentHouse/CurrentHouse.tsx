e2896b0c6280b79022785887b456a587b0f93ddf
Harry <nthai7@cmcglobal.vn>
2023-03-09 10:23:49 +0700

    fix: update the logic and display the table sort student

--- src/webparts/hogwartsShort/components/CurrentHouse/CurrentHouse.tsx
+++ src/webparts/hogwartsShort/components/CurrentHouse/CurrentHouse.tsx
@@ -1,17 +1,53 @@
-import { Button } from "@mui/material";
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  tableCellClasses,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 import { SPFI } from "@pnp/sp";
 import * as React from "react";
 import { useEffect, useState } from "react";
 import { getSP } from "../../../../pnpConfig";
+import { IStudentData } from "../IHogwartsShortProps";
 import { ICurrentHouse } from "./ICurrentHouse";
+import { styled } from "@mui/material/styles";
 
 const HouseList = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"];
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: "#ccc",
+    color: theme.palette.common.black,
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
 
 const CurrentHouse = (props: ICurrentHouse) => {
   const { studentData, context } = props;
   const [currentHouse, _setCurrentHouse] = useState<string>(
     studentData && studentData.CurrentHouse ? studentData.CurrentHouse : ""
   );
+  const [_listStudent, _setListStudent] = useState<IStudentData[]>([]);
+  const [isChangeHouse, setIsChangeHouse] = useState<boolean>(false);
 
   let _sp: SPFI = getSP(context);
   const LIST_NAME = "Hogwarts Students";
@@ -36,6 +72,9 @@ const CurrentHouse = (props: ICurrentHouse) => {
         .update({
           CurrentHouse: currentHouse,
           CurrentDateandTime: new Date(),
+        })
+        .then(() => {
+          setIsChangeHouse(true);
         });
     } catch (error) {
       console.log(error);
@@ -51,6 +90,35 @@ const CurrentHouse = (props: ICurrentHouse) => {
       _setCurrentHouse(studentData.CurrentHouse);
     }
   }, [studentData]);
+
+  useEffect(() => {
+    if (!studentData && currentHouse === "" && isChangeHouse === false) {
+      return;
+    } else {
+      _sp.web.lists
+        .getByTitle(LIST_NAME)
+        .items.filter(`CurrentHouse eq '${currentHouse}'`)
+        .orderBy("FullName", true)()
+        .then((items) => {
+          _setListStudent(
+            items.map((item: IStudentData) => {
+              return {
+                Id: item.Id,
+                Title: item.Title,
+                Email: item.Email,
+                FullName: item.FullName,
+                CurrentHouse: item.CurrentHouse,
+                CurrentDateandTime: item.CurrentDateandTime,
+              };
+            })
+          );
+          setIsChangeHouse(false);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  }, [currentHouse, isChangeHouse]);
 
   return (
     <div>
@@ -91,6 +159,78 @@ const CurrentHouse = (props: ICurrentHouse) => {
           </Button>
         )}
       </div>
+      <hr />
+      {currentHouse !== "" && (
+        <>
+          <h1>
+            Members in
+            <span
+              style={{
+                color: "red",
+                fontWeight: "bold",
+              }}
+            >
+              {" "}
+              {currentHouse}
+              {"'s "}
+            </span>
+            house:
+          </h1>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <StyledTableCell
+                    sx={{
+                      fontWeight: "bold",
+                      color: "black",
+                    }}
+                  >
+                    Full Name
+                  </StyledTableCell>
+                  <StyledTableCell
+                    align="center"
+                    sx={{
+                      fontWeight: "bold",
+                      color: "black",
+                    }}
+                  >
+                    Email
+                  </StyledTableCell>
+                  <StyledTableCell align="center">
+                    Current House
+                  </StyledTableCell>
+                  <StyledTableCell align="right">
+                    Join Date and Time
+                  </StyledTableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {_listStudent.map((student) => (
+                  <StyledTableRow
+                    key={student.FullName}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <StyledTableCell component="th" scope="row">
+                      {student.FullName}
+                    </StyledTableCell>
+                    <StyledTableCell align="center">
+                      {student.Email}
+                    </StyledTableCell>
+                    <StyledTableCell align="center">
+                      {student.CurrentHouse}
+                    </StyledTableCell>
+                    <StyledTableCell align="right">
+                      {student.CurrentDateandTime}
+                    </StyledTableCell>
+                  </StyledTableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </>
+      )}
+      <hr />
     </div>
   );
 };
